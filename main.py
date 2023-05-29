3bc663360aaa93c280de10e17e84eb7ee27882da
jwhitlow45 <jwhitlow45@gmail.com>
2023-05-29 02:26:57 -0700

    Add settings manager and clean out template artifacts

--- main.py
+++ main.py
@@ -4,38 +4,25 @@ import os
 # For easy intellisense checkout the decky-loader code one directory up
 # or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
+from decky_plugin import logger
+from settings import SettingsManager
 
+settingsDir = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
+settings = SettingsManager(name="settings", settings_directory=settingsDir)
 
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
-
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
-    async def _main(self):
-        decky_plugin.logger.info("Hello World!")
-
-    # Function called first during the unload process, utilize this to handle your plugin being removed
-    async def _unload(self):
-        decky_plugin.logger.info("Goodbye World!")
-        pass
-
-    # Migrations that should be performed before entering `_main()`.
-    async def _migration(self):
-        decky_plugin.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+    # Expose settings methods to allow them to be called from typescript
+    # Credit to the decky-autosuspend plugin for this snippet
+    # https://github.com/jurassicplayer/decky-autosuspend/blob/main/main.py
+    async def settings_read(self):
+        logger.info('Reading settings')
+        return settings.read()
+    async def settings_commit(self):
+        logger.info('Saving settings')
+        return settings.commit()
+    async def settings_getSetting(self, key: str, defaults):
+        logger.info('Get {}'.format(key))
+        return settings.getSetting(key, defaults)
+    async def settings_setSetting(self, key: str, value):
+        logger.info('Set {}: {}'.format(key, value))
+        return settings.setSetting(key, value)
