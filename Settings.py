00f184619a76ba9ecef8258192080eef2281c808
jwhitlow45 <jwhitlow45@gmail.com>
2023-05-29 13:46:38 -0700

    Add settings class to handle modifying local json user settings file

---
+++ Settings.py
@@ -0,0 +1,71 @@
+import json
+import os
+from decky_plugin import logger
+
+DEF_DAYS = 0
+DEF_HOURS = 12
+DEF_MINUTES = 0
+DEF_NOTIFY_FOREVER_GAMES = 1
+DEF_NOTIFY_TRIAL_GAMES = 1
+
+__SETTINGS_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+SETTINGS_FOLDER_NAME = 'settings'
+SETTINGS_FILE_NAME = 'user_settings.json'
+SETTINGS_FILE_PATH = os.path.join(__SETTINGS_FILE_DIR, SETTINGS_FOLDER_NAME, SETTINGS_FILE_NAME)
+
+class UpdateFrequency:
+    def __init__(self, days: int = DEF_DAYS, hours: int = DEF_HOURS, minutes: int = DEF_MINUTES):
+        self.days = days
+        self.hours = hours
+        self.minutes = minutes
+
+class UpdateFrequencyEncoder(json.JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+    
+class Settings:
+    def __init__(self,
+                 update_frequency: UpdateFrequency = UpdateFrequency(),
+                 notify_forever_games: int = DEF_NOTIFY_FOREVER_GAMES,
+                 notify_trial_games: int = DEF_NOTIFY_TRIAL_GAMES):
+        self.update_frequency = update_frequency
+        self.notify_forever_games = notify_forever_games
+        self.notify_trial_games = notify_trial_games
+    
+    def read_settings(self):
+        if not os.path.exists(SETTINGS_FILE_PATH) or not os.path.isfile(SETTINGS_FILE_PATH):
+            self.restore_settings()
+        try:
+            with open(SETTINGS_FILE_PATH, 'r') as file:
+                user_settings = json.load(file)
+                self.__parse_settings(user_settings)
+        except Exception as e:
+            logger.info(f'Exception while trying to read settings file: {e}')
+            self.restore_settings()
+        logger.info('Read user settings')
+    
+    def commit_settings(self):
+        with open(SETTINGS_FILE_PATH, 'w') as file:
+            file.seek(0)
+            json.dump(vars(self), file, indent=4, cls=UpdateFrequencyEncoder)
+            file.truncate()
+        logger.info('Saved changes to settings file')
+    
+    def restore_settings(self):
+        defaults = Settings()
+        with open(SETTINGS_FILE_PATH, 'w') as file:
+            file.seek(0)
+            json.dump(vars(defaults), file, indent=4, cls=UpdateFrequencyEncoder)
+            file.truncate()
+        logger.info('Restored settings file')
+        
+    def __parse_settings(self, user_settings):
+        # Read update frequency
+        update_frequency = user_settings.get('update_frequency', UpdateFrequency())
+        self.update_frequency.days = update_frequency.get('days', DEF_DAYS)
+        self.update_frequency.hours = update_frequency.get('hours', DEF_HOURS)
+        self.update_frequency.minutes = update_frequency.get('minutes', DEF_MINUTES)
+        
+        # Read notification settings
+        self.notify_forever_games = user_settings.get('notify_forever_games', DEF_NOTIFY_FOREVER_GAMES)
+        self.notify_trial_games = user_settings.get('notify_trial_games', DEF_NOTIFY_TRIAL_GAMES)
