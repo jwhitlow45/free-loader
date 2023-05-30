83f1f39370db3ac9ac59bf9016cca0efb37e98b0
jwhitlow45 <jwhitlow45@gmail.com>
2023-05-30 10:58:40 -0700

    broken:toggle button logic has been fixed, but settings are loaded on each button change which needs to be investigated

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -7,38 +7,48 @@ enum Settings {
   NOTIFY_TRIAL_GAMES = 'notify_trial_games'
 }
 
+let cur_settings = {}
+const MAX_LOAD_SETTINGS_RETRIES = 3;
+const RETRY_COOLDOWN = 1000;
+
+async function loadSettings(retries: number = 0) {
+  // wait for 1 second to give time for loading
+  if (retries > MAX_LOAD_SETTINGS_RETRIES) {
+    PyCaller.logger(`Max retries of ${MAX_LOAD_SETTINGS_RETRIES} reached for loading settings.`);
+    return false;
+  }
+
+  for (let item in Settings) {
+    let response = await PyCaller.getSetting(Settings[item]);
+    if (response.success) {
+      cur_settings[Settings[item]] = !!response.result;
+    } else {
+      PyCaller.logger(`Cannot load settings...retrying in ${RETRY_COOLDOWN/1000} second(s).`);
+      await new Promise(f => setTimeout(f, RETRY_COOLDOWN));
+      return loadSettings(retries + 1);
+    }
+  }
+  return true;
+}
+
 const SettingsPanel: React.FunctionComponent = () => {
 
-  const [settings, setSettings] = useState({});
-  const [isSettingsLoaded, setSettingsLoaded] = useState(false);
-  const [notifyForeverGames, setNotifyForeverGames] = useState(false);
-  const [notifyTrialGames, setNotifyTrialGames] = useState(false);
-
-  let cur_settings = {}
+  let [notifyForeverGames, setNotifyForeverGames] = useState(false);
+  let [notifyTrialGames, setNotifyTrialGames] = useState(false);
 
   useEffect(() => {
-    async function loadSettings() {
-      for (let item in Settings) {
-        let response = await PyCaller.getSetting(Settings[item]);
-        if (response.success) {
-          cur_settings[Settings[item]] = !!response.result;
-        } else {
-          setSettingsLoaded(false);
-          break;
-        }
-        setSettingsLoaded(true);
+    loadSettings().then((loaded) => {
+      if (loaded) {
+        PyCaller.logger('Loaded settings:');
+        PyCaller.logger(cur_settings);
+        setNotifyForeverGames(cur_settings[Settings.NOTIFY_FOREVER_GAMES]);
+        setNotifyTrialGames(cur_settings[Settings.NOTIFY_TRIAL_GAMES]);
+      } else {
+        PyCaller.logger('Could not load settings...restoring settings file.');
+        PyCaller.restoreSettings();
       }
-
-      PyCaller.logger('Loaded settings:');
-      PyCaller.logger(cur_settings);
-      setNotifyForeverGames(cur_settings[Settings.NOTIFY_FOREVER_GAMES]);
-      setNotifyTrialGames(cur_settings[Settings.NOTIFY_TRIAL_GAMES]);
-    }
-
-    if (!isSettingsLoaded) {
-      loadSettings()
-    }
-  })
+    });
+  });
 
   return (
     <PanelSection title="Settings">
@@ -48,10 +58,10 @@ const SettingsPanel: React.FunctionComponent = () => {
           checked={notifyForeverGames}
           layout='below'
           onChange={async () => {
-            settings[Settings.NOTIFY_FOREVER_GAMES] = !settings[Settings.NOTIFY_FOREVER_GAMES];
+            cur_settings[Settings.NOTIFY_FOREVER_GAMES] = !cur_settings[Settings.NOTIFY_FOREVER_GAMES];
             await PyCaller.setSetting(
               Settings.NOTIFY_FOREVER_GAMES,
-              settings[Settings.NOTIFY_FOREVER_GAMES]);
+              cur_settings[Settings.NOTIFY_FOREVER_GAMES]);
         }}></ToggleField>
         <ToggleField
           label='Notify on Trial Games'
@@ -62,14 +72,20 @@ const SettingsPanel: React.FunctionComponent = () => {
             await PyCaller.setSetting(
               Settings.NOTIFY_TRIAL_GAMES,
               cur_settings[Settings.NOTIFY_TRIAL_GAMES]);
+            setNotifyTrialGames(cur_settings[Settings.NOTIFY_TRIAL_GAMES]);
+            PyCaller.logger('button');
+            PyCaller.logger(notifyTrialGames);
+            PyCaller.logger(cur_settings);
         }}>Notify on Trial Games</ToggleField>
         <ButtonItem layout='below' onClick={async () => {
           await PyCaller.restoreSettings();
         }}>Restore Settings</ButtonItem>
         <ButtonItem layout='below' onClick={async () => {
-          let response = await PyCaller.getSettings();
-          PyCaller.logger(response.success);
-          PyCaller.logger(response.result);
+          let forever = await PyCaller.getSetting(Settings.NOTIFY_FOREVER_GAMES);
+          let trial = await PyCaller.getSetting(Settings.NOTIFY_TRIAL_GAMES);
+          
+          await PyCaller.logger(forever.result);
+          await PyCaller.logger(trial.result);
         }}>Test Button</ButtonItem>
       </PanelSectionRow>
     </PanelSection>
