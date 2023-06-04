8d8873d1982bdd2284c9b127abb34e61911671fc
jwhitlow45 <jwhitlow45@gmail.com>
2023-06-04 13:23:34 -0700

    Add settings enum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from enum import Enum
+from time import sleep
 from settings import SettingsManager
 from decky_plugin import logger
 # Get environment variable
@@ -7,15 +9,28 @@ settingsDir = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
 logger.info('Settings path: {}'.format(os.path.join(settingsDir, 'settings.json')))
 settings = SettingsManager(name="settings", settings_directory=settingsDir)
 
+class Settings(Enum):
+    UPDATE_FREQ_DAY = "update_frequency_day"
+    UPDATE_FREQ_HOUR = "update_frequency_hour"
+    UPDATE_FREQ_MIN = "update_frequency_min"
+    NOTIFY_FOREVER_GAMES = "notify_forever_games"
+    NOTIFY_TRIAL_GAMES = "notify_trial_games"
+    
 SETTINGS_DEFAULTS = {
-    'update_frequency_day': 0,
-    'update_frequency_hour': 12,
-    'update_frequency_min': 0,
-    'notify_forever_games': True,
-    'notify_trial_games': True,
+    Settings.UPDATE_FREQ_DAY.value : 0,
+    Settings.UPDATE_FREQ_HOUR.value : 12,
+    Settings.UPDATE_FREQ_MIN.value : 0,
+    Settings.NOTIFY_FOREVER_GAMES.value : True,
+    Settings.NOTIFY_TRIAL_GAMES.value : True,
 }
 
 class Plugin:
+    async def _main(self):
+        # while True:
+        #     sleep(3)
+        #     await self.log(self, info='hello world')
+        pass
+    
     async def settings_commit(self):
         logger.info('Saving settings')
         return settings.commit()
