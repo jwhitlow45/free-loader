5fe7353de62b75028e70383423925a40f602b3ce
jwhitlow45 <jwhitlow45@gmail.com>
2023-09-16 14:15:23 -0700

    bug:Fixed wrapped text overlapping on previous line in game title

--- src/components/GamePanel.tsx
+++ src/components/GamePanel.tsx
@@ -24,7 +24,7 @@ const GamePanel: React.FunctionComponent<GamePanelProps> = (props) => {
               </td>
               <td style={{width: '100%'}}>
                 <div>
-                  <h1>{props.title}</h1>
+                  <h1 style={{ lineHeight: '40px' }}>{props.title}</h1>
                   <h3><s>{props.worth}</s> Free</h3>
                   <h3>Ends {props.end_date}</h3>
                 </div>
