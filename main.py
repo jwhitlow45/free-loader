e98f7749f5b4725006f4b700df54b4c40298b8bb
jwhitlow45 <jwhitlow45@gmail.com>
2025-01-11 16:54:15 -0800

    chore:log exceptions from core components, format file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath('../plugins/free-loader'))
+sys.path.append(os.path.abspath("../plugins/free-loader"))
 
-from enum import Enum
-from settings import SettingsManager
 from decky_plugin import logger
 from py_modules.deal_db import DealDB
 from py_modules.settings import settingsManager, SETTINGS_DEFAULTS
@@ -12,56 +10,76 @@ from py_modules.settings import settingsManager, SETTINGS_DEFAULTS
 
 class Plugin:
     async def _main(self):
-        # check settings for validity, if a setting is malformed then set it to default
-        for key, value in SETTINGS_DEFAULTS.items():
-            setting = await Plugin.settings_getSetting(self, key)
-            if setting == None:
-                logger.info(f'Setting {key} is malformed...resetting to default.')
-                await Plugin.settings_setSetting(self, key, value)
-    
+        try:
+            # check settings for validity, if a setting is malformed then set it to default
+            for key, value in SETTINGS_DEFAULTS.items():
+                setting = await Plugin.settings_getSetting(self, key)
+                if setting is None:
+                    logger.info(f"Setting {key} is malformed...resetting to default.")
+                    await Plugin.settings_setSetting(self, key, value)
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
     async def settings_commit(self):
-        logger.info('Saving settings')
+        logger.info("Saving settings")
         return settingsManager.commit()
 
     async def settings_getSetting(self, key: str):
-        logger.info('Get {}'.format(key))
+        logger.info("Get {}".format(key))
         return settingsManager.getSetting(key)
 
     async def settings_setSetting(self, key: str, value):
-        logger.info('Set {}: {}'.format(key, value))
+        logger.info("Set {}: {}".format(key, value))
         return settingsManager.setSetting(key, value)
-    
+
     async def settings_restoreSettings(self):
         for key, value in SETTINGS_DEFAULTS.items():
             settingsManager.setSetting(key, value)
-        logger.info('Restored settings to defaults')
+        logger.info("Restored settings to defaults")
         return settingsManager.commit()
-    
+
     async def update_deals_now(self):
-        dealdb = DealDB()
-        dealdb.process_new_deals()
-        return dealdb.num_new_deals
-        
+        try:
+            dealdb = DealDB()
+            dealdb.process_new_deals()
+            return dealdb.num_new_deals
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
     async def read_deals(self) -> dict:
-        logger.info('Reading deals from local json')
-        dealdb = DealDB()
-        dealdb.import_from_json()
-        return dealdb.deals
-    
+        try:
+            logger.info("Reading deals from local json")
+            dealdb = DealDB()
+            dealdb.import_from_json()
+            return dealdb.deals
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
     async def clear_deals(self) -> dict:
-        dealdb = DealDB()
-        dealdb.export_to_json()
-        logger.info('Cleared game database')
-        return dealdb.deals
-    
+        try:
+            dealdb = DealDB()
+            dealdb.export_to_json()
+            logger.info("Cleared game database")
+            return dealdb.deals
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
     async def toggle_deal_visibility(self, id: str) -> dict:
-        dealdb = DealDB()
-        is_hidden = dealdb.toggle_deal_visibility(id)
-        logger.info(f'Game id {id} hidden: {is_hidden}')
-        return { 'hidden': is_hidden }
-    
+        try:
+            dealdb = DealDB()
+            is_hidden = dealdb.toggle_deal_visibility(id)
+            logger.info(f"Game id {id} hidden: {is_hidden}")
+            return {"hidden": is_hidden}
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
     async def logger_info(self, info):
         logger.info(info)
-        
+
     async def logger_error(self, error):
         logger.error(error)
