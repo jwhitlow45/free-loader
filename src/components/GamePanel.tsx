7ea2f70c03b8ee485cab2350d506f44a15718e61
jwhitlow45 <jwhitlow45@gmail.com>
2025-06-15 14:20:52 -0700

    refactor:formatting

--- src/components/GamePanel.tsx
+++ src/components/GamePanel.tsx
@@ -20,44 +20,44 @@ const GamePanel: React.FunctionComponent<GamePanelProps> = (props) => {
   const [hideGamePanel, setHideGamePanel] = React.useState(!props.show_hidden_game && props.hidden);
   if (hideGamePanel) return null;
   return (
-    <div style={{display: 'flex', marginBottom: '10px'}}>
-    <PanelSectionRow>
-      <DialogButton
-        onClick={async () => {
-          Navigation.CloseSideMenus()
-          Navigation.NavigateToExternalWeb(props.link);
-        }}
-        onSecondaryButton={async () => {
-          let response = await PyCaller.toggleDealVisibility(props.id)
-          if (response.success) {
-            let hidden_state = Boolean(response.result['hidden'])
-            setHiddenState(hidden_state)
-            setHideGamePanel(!props.show_hidden_game && hidden_state)
-          }
-        }}
-        onOKActionDescription='Open Store Page'
-        onSecondaryActionDescription={hiddenState ? 'Show Game' : 'Hide Game'}
-      >
-        <table>
-          <tr>
-            <td style={{ width: '100%' }}>
-              <img src={props.image_url} style={{ borderRadius: '10px', width: '100%' }} />
-            </td>
-          </tr>
-          <tr>
-            <td style={{ width: '100%' }}>
-              <div>
-                {props.show_title && <h3 style={{ lineHeight: '20px' }}>{props.title}</h3>}
-                <h4 style={{ lineHeight: '3px' }}>{props.platforms}</h4>
-                <h4 style={{ lineHeight: '3px' }}><s>{props.worth}</s> Free</h4>
-                <h4 style={{ lineHeight: '3px' }}>Ends {props.end_date}</h4>
-                {hiddenState && <h4 style={{ lineHeight: '3px' }}><i>Hidden</i></h4>}
-              </div>
-            </td>
-          </tr>
-        </table>
-      </DialogButton>
-    </PanelSectionRow>
+    <div style={{ display: 'flex', marginBottom: '10px' }}>
+      <PanelSectionRow>
+        <DialogButton
+          onClick={async () => {
+            Navigation.CloseSideMenus()
+            Navigation.NavigateToExternalWeb(props.link);
+          }}
+          onSecondaryButton={async () => {
+            let response = await PyCaller.toggleDealVisibility(props.id)
+            if (response.success) {
+              let hidden_state = Boolean(response.result['hidden'])
+              setHiddenState(hidden_state)
+              setHideGamePanel(!props.show_hidden_game && hidden_state)
+            }
+          }}
+          onOKActionDescription='Open Store Page'
+          onSecondaryActionDescription={hiddenState ? 'Show Game' : 'Hide Game'}
+        >
+          <table>
+            <tr>
+              <td style={{ width: '100%' }}>
+                <img src={props.image_url} style={{ borderRadius: '10px', width: '100%' }} />
+              </td>
+            </tr>
+            <tr>
+              <td style={{ width: '100%' }}>
+                <div>
+                  {props.show_title && <h3 style={{ lineHeight: '20px' }}>{props.title}</h3>}
+                  <h4 style={{ lineHeight: '3px' }}>{props.platforms}</h4>
+                  <h4 style={{ lineHeight: '3px' }}><s>{props.worth}</s> Free</h4>
+                  <h4 style={{ lineHeight: '3px' }}>Ends {props.end_date}</h4>
+                  {hiddenState && <h4 style={{ lineHeight: '3px' }}><i>Hidden</i></h4>}
+                </div>
+              </td>
+            </tr>
+          </table>
+        </DialogButton>
+      </PanelSectionRow>
     </div>
   );
 }
