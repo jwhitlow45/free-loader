60e8abc30a603b2518be05c0a57ac6f0473a95a9
jwhitlow45 <jwhitlow45@gmail.com>
2025-11-26 18:28:07 -0800

    feat:add viewable qr code to game panels

--- src/components/GamePanel.tsx
+++ src/components/GamePanel.tsx
@@ -19,9 +19,23 @@ type GamePanelProps = {
 
 const GamePanel: React.FunctionComponent<GamePanelProps> = (props) => {
   const [hiddenState, setHiddenState] = React.useState(props.hidden);
+  const [showQrCode, setShowQrCode] = React.useState(false);
   const [hideGamePanel, setHideGamePanel] = React.useState(!props.show_hidden_game && props.hidden);
 
   const { setGamesList } = useContext(GamesListContext);
+
+  function getQrCodeImageUrl() {
+    const encodeQueryParams = (p: { [key: string]: string | number | boolean }) =>
+      Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+
+    const baseUrl = 'https://api.qrserver.com/v1/create-qr-code/?'
+    const queryParams = {
+      size: `150x150`,
+      data: props.link
+    }
+    return baseUrl + encodeQueryParams(queryParams)
+  }
+  const qrCodeUrl = getQrCodeImageUrl();
 
   if (hideGamePanel) return null;
   return (
@@ -32,6 +46,7 @@ const GamePanel: React.FunctionComponent<GamePanelProps> = (props) => {
             Navigation.CloseSideMenus()
             Navigation.NavigateToExternalWeb(props.link);
           }}
+          onOKActionDescription='Open Store Page'
           onSecondaryButton={async () => {
             let response = await PyCaller.toggleDealVisibility(props.id)
             if (response.success) {
@@ -41,27 +56,25 @@ const GamePanel: React.FunctionComponent<GamePanelProps> = (props) => {
               setGamesList(await fetchGamesList())
             }
           }}
-          onOKActionDescription='Open Store Page'
           onSecondaryActionDescription={hiddenState ? 'Show Game' : 'Hide Game'}
+          onOptionsButton={() => {
+            setShowQrCode(!showQrCode)
+          }}
+          onOptionsActionDescription={showQrCode ? 'Hide QR Code' : 'Show QR Code'}
         >
-          <table>
-            <tr>
-              <td style={{ width: '100%' }}>
-                <img src={props.image_url} style={{ borderRadius: '10px', width: '100%' }} />
-              </td>
-            </tr>
-            <tr>
-              <td style={{ width: '100%' }}>
-                <div>
-                  {props.show_title && <h3 style={{ lineHeight: '20px' }}>{props.title}</h3>}
-                  <h4 style={{ lineHeight: '3px' }}>{props.platforms}</h4>
-                  <h4 style={{ lineHeight: '3px' }}><s>{props.worth}</s> Free</h4>
-                  <h4 style={{ lineHeight: '3px' }}>Ends {props.end_date}</h4>
-                  {hiddenState && <h4 style={{ lineHeight: '3px' }}><i>Hidden</i></h4>}
-                </div>
-              </td>
-            </tr>
-          </table>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{ display: 'flex', margin: 'auto' }}>
+              <img src={props.image_url} hidden={showQrCode} style={{ height: '105px', borderRadius: '10px', animation: 'fadeIn 0.5s ease-in-out' }} />
+              <img src={qrCodeUrl} hidden={!showQrCode} style={{ height: '105px', padding: '0 59.8255814px', animation: 'fadeIn 0.5s ease-in-out' }} />
+            </div>
+            <div style={{ width: '100%', marginTop: '8px' }}>
+              {props.show_title && <h3 style={{ lineHeight: '20px' }}>{props.title}</h3>}
+              <h4 style={{ lineHeight: '3px' }}>{props.platforms}</h4>
+              <h4 style={{ lineHeight: '3px' }}><s>{props.worth}</s> Free</h4>
+              <h4 style={{ lineHeight: '3px' }}>Ends {props.end_date}</h4>
+              {hiddenState && <h4 style={{ lineHeight: '3px' }}><i>Hidden</i></h4>}
+            </div>
+          </div>
         </DialogButton>
       </PanelSectionRow>
       <style>
